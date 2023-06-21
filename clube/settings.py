8d99d47eefaa1b4be3941ab0f3591304d32e2580
Gustavo Santos <gus.gabriel@hotmail.com>
2023-06-21 14:29:18 +0200

    secret key

--- clube/settings.py
+++ clube/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-z(vn9b=3-^8_(cf@oo2!1glpsa!6s!-#c0o(+p!)m4krcm=c&a"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
